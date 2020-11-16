5282bf6e50f05c90305513f174cf039d43ede76b
Trinh Tran <Trinh.Tran@fitlg.asia>
2020-11-16 17:54:29 +0700

    fix saving file

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ const store = new MongoDBStore({
 const csrfProtection = csrf();
 
 const fileStorage = multer.diskStorage({
-    description: (req, file, cb) => {
+    destination: (req, file, cb) => {
         cb(null, 'images')
     },
     filename: (req, file, cb) => {
@@ -47,7 +47,8 @@ app.set("views", "views"); // the second param is the name of folder "views"
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(multer({ storage: fileStorage, fileFilter: fileFilter }).single('image'));
-app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.static(path.join(__dirname, 'public')));
+app.use('/images', express.static(path.join(__dirname, 'images')))
 app.use(session({
     secret: "my secret",
     resave: false,
